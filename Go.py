dc5758964ab33e00592a1785dd88bbc73fa8eae2
WillieXia <WillieXia@users.noreply.github.com>
2019-12-05 16:18:50 -0500

    Completed Board

--- Go.py
+++ Go.py
@@ -9,29 +9,58 @@ import pygame
 
 pygame.init()
 
-class Board(pygame.sprite.Sprite):
-    def __init__(self, image_file, location):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(image_file)
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
+class Board:
+    def __init__(self):
+        self.grid = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
+class whitePiece:
+    def __init__(self):
+        self.image = pygame.image.load("white_piece.png")
 
-displayDimensions = (800,800)
+displayDimensions = (1000,1000)
 display = pygame.display.set_mode(displayDimensions)
 
 clock = pygame.time.Clock()
-background_image = pygame.image.load("go_board.png").convert()
+background_image = pygame.image.load("go_board.png")
+
+
 
 gameover = False
 
-clock_tick_rate=20
-while(gameover==False):
+display.blit(background_image, [0, 0])
+board = Board()
+while not gameover:
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            dead = True
+            gameover = True
 
-    display.blit(background_image, [0, 0])
-    print("0")
-    pygame.display.flip()
-    clock.tick(clock_tick_rate)
+    if(pygame.mouse.get_pressed()[0]):
+        coord = pygame.mouse.get_pos()
+        white = pygame.image.load("white_piece.png").convert_alpha()
+        
+        display.blit(white, (coord[0]-24,coord[1]-20))
+
+    if(pygame.mouse.get_pressed()[2]):
+        coord = pygame.mouse.get_pos()
+        black = pygame.image.load("black_piece.png").convert_alpha()
+        
+        display.blit(black, (coord[0]-28, coord[1]-28))
